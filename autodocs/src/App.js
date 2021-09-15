af01103103c2ba4c50309d00b8bc8e9d9e5c19a7
Marcelo Uchôas <marcelouchoas@gmail.com>
2021-09-14 23:32:39 -0300

    Inicio menu de seleção

---
+++ autodocs/src/App.js
@@ -0,0 +1,29 @@
+//import logo from './logo.svg';
+import React from 'react';
+//import ReactDOM from 'react-dom';
+//import 'antd/dist/antd.css';
+import './App.css';
+import { Button } from 'antd';
+
+
+
+function App() {
+  return (
+  document.getElementById('container'),
+    <div className="App">
+      <header className="App-header"> 
+
+      </header>
+      <div>
+        <>
+        <tr id = "menu">
+          <td><Button type="primary" >Primary Button</Button> </td>
+          <td><Button type="primary" >Primary Button</Button> </td>
+          <td><Button type="primary" >Primary Button</Button> </td>
+        </tr>
+        </>
+      </div>
+    </div>
+  );
+}
+export default App;
